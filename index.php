ca91b19d452efd13b3bbb2df4ce283ae821ad4ea
Dan iel <6431503087@lamduan.mfu.ac.th>
2024-02-15 22:46:43 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php 
+/**
+ **** AppzStory Admin Ajax ****
+ * 
+ * @link https://appzstory.dev
+ * @author Yothin Sapsamran (Jame AppzStory Studio)
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>SE Admin</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <!-- Favicons -->
+ <link rel="apple-touch-icon" sizes="180x180" href="../../assets/img/Logo.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="../../assets/img/Logo.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="../../assets/img/Logo.png">
+ <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="theme-color" content="#ffffff">
+
+  <!-- stylesheet -->
+  <link href="https://fonts.googleapis.com/css?family=Mali" rel="stylesheet">
+  <link rel="stylesheet" href="admin/assets/css/adminlte.min.css">
+  <link rel="stylesheet" href="admin/assets/css/style.css">
+  <link rel="stylesheet" href="admin/plugins/toastr/toastr.min.css">
+
+  <link rel="stylesheet" href="admin/assets/css/style.css">
+</head>
+<body>
+<div class="bg"></div>
+<div class="container h-100">
+  <div class="row justify-content-center align-items-center h-100">
+    <div class="col-md-6">
+      <div class="card shadow p-5">
+        <div class="card-header"> 
+          <h3 class="text-center font-weight-bold"> SE LAB </h3>
+        </div>
+        <div class="card-body py-5">
+            <a class="btn btn-primary btn-block text-white" href="admin/login.php"> เข้าสู่หน้าผู้ดูแลระบบ </a>
+        </div>
+        <footer class="text-secondary text-center">
+            สอนเขียนเว็บไซต์ด้วยตัวเอง <span class="text-pink">♥️</span>
+            <a class="border-primary" href="https://appzstory.dev" target="_blank"></a>
+        </footer>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- SCRIPTS -->
+<script src="admin/plugins/jquery/jquery.min.js"></script>
+<script src="admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="admin/plugins/toastr/toastr.min.js"></script>
+
+</body>
+</html>
